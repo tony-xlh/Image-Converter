3bf5ec8904b781e492c0765adfe9e3193ea8d57e
xulihang <xulihanghai@163.com>
2023-07-26 10:59:33 +0800

    add vite example

---
+++ example/vite/src/main.ts
@@ -0,0 +1,4 @@
+import { ImageConverter } from "batch-image-converter";
+
+let container = document.querySelector<HTMLDivElement>('#app');
+new ImageConverter({container:container!,license:"DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="});
